82a74a85877725cb3dac19fa71caeba9b42ccded
Sébastien HU <sehu@laposte.net>
2015-11-22 19:13:47 +0100

    ajout de l'ArrayAdapter (Q4)

--- java/Sudoku.java
+++ java/Sudoku.java
@@ -1,38 +1,31 @@
 package com.potkanburger.sudoku_durif_genoux_hu;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
 
 
-public class Sudoku extends ActionBarActivity {
+public class Sudoku extends Activity {
+
+    GridView grille = null;
+    TextView item_sudoku = null;
+    ArrayAdapter<String> arrayAdapter = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sudoku);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_sudoku, menu);
-        return true;
-    }
+        grille = (GridView) findViewById(R.id.grile);
+        item_sudoku = (TextView) findViewById(R.id.item_sudoku);
+        arrayAdapter = new ArrayAdapter<String>(this, R.layout.item_sudoku);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        for(int i=1;i<=81;i++){
+            arrayAdapter.add(String.valueOf(i));
         }
-
-        return super.onOptionsItemSelected(item);
+        grille.setAdapter(arrayAdapter);
     }
 }
